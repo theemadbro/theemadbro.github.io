8859a1418c9bf5b37efa4e11fdd0722e28afedf0
Kevin Capotosto <36879680+theemadbro@users.noreply.github.com>
2019-02-17 07:56:13 -0800

    Tons of updates to most of the main landings

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
 $(document).ready(function(){
-    console.log("Hello, you sneaky fella you. I'm just starting setting up javascript for a more fluid experience on my website. Here's a little treat if you felt like checking this out! :D")
-    $(document).on('click', '.navitem', function(e){
-        console.log("navbar clicked!")
-        e.preventDefault();
-    });
+    console.log("Hello, you sneaky fella you. I'm just starting setting up javascript for a more fluid experience on my website. Thanks for checking my little portfolio, and in general looking into little 'ol me.")
 });
