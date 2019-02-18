c19f42a60200e36a63d3802101b76363dc3c0d40
Kevin Capotosto <36879680+theemadbro@users.noreply.github.com>
2019-02-17 19:29:47 -0800

    photos page fixed up, updated resume and added to about page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
     console.log("Hello, you sneaky fella you. I'm just starting setting up javascript for a more fluid experience on my website. Thanks for checking my little portfolio, and in general looking into little 'ol me.")
+    $('.photos').hover(function () { 
+        console.log("HOVERED")
+    });
+    $('.photos img').click(function () { 
+        console.log("clicked");
+
+        var imgId = ($(this).attr('src'))
+        console.log(imgId);
+       
+        $('#full-photo-id').attr('src', "./originals/"+imgId);
+    });
+
+    $('.modal').on('hide.bs.modal', function (e) {
+        $('#full-photo-id').attr('src', "");
+      })
 });
